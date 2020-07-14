cca7ec911d6ab4ae5970d9cdb6070bf4adbebf79
Gabriel Voicu <Gabriel_Voicu@dell.com>
2020-07-14 10:35:18 +0300

    add more

--- src/main/java/org/mrc/exercises/Lesson2/Employee.java
+++ src/main/java/org/mrc/exercises/Lesson2/Employee.java
@@ -5,7 +5,31 @@ public class Employee {
     private String name;
     private boolean isActive;
 
+    public Employee(int id) {
+        this.id = id;
+    }
+
     public boolean isActive() {
         return isActive;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
     }
 }
--- src/main/java/org/mrc/exercises/lesson1/Lesson1.java
+++ src/main/java/org/mrc/exercises/lesson1/Lesson1.java
@@ -1,11 +1,28 @@
 package org.mrc.exercises.lesson1;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * A se urmari trecerea testelor
  */
 public class Lesson1 {
+    private static Logger logger = Logger.getLogger("marcel");
+
+    /**
+     * 0. Scrie o metoda care returneaza un String si primeste ca parametrii un string numit name si un numeral numit age
+     * metoda va afisa pe ecran folosid loggerul prezent din aceasta clasa (logger.info(...)) numele si varsta frumos.
+     *  Sa se apeleaza aceasta metoda dintr-o metoda main scrisa tot in aceasta clasa
+     */
+
+
+    /**
+     * 0.1. Scrie o metoda care returneaza un List de Employee si primeste ca parametru un Employee
+     *  metoda va fi apelata din metoda Main cu un employee care va avea doar id iar in metoda
+     *  ii va seta nume si isActive true, apoi va fi adaugat in lista ce va fi returnata
+     */
+
+
     /**
      * 1. Sa se calculeze suma tuturor salariilor primite si sa returneze suma daca totul este in regula
      * daca lista de salarii este mai mica de 4 sa se arunce exceptia SalariesListIsTooSmallException
